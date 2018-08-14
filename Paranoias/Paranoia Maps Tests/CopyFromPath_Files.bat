ee6999327ca581799f38237ced9ff5024b7effd3
Osvaldo Martini <OsvaldoMartini@users.noreply.github.com>
2018-08-14 09:17:03 +0100

    Paranoia Maps Tests

---
+++ Paranoias/Paranoia Maps Tests/CopyFromPath_Files.bat
@@ -0,0 +1,3 @@
+xcopy /Y "C:\Workspace\CMP\Main\Source\Web\UI\Assets\Javascript\Maps\MapDrawing.js" 
+xcopy /Y "C:\Workspace\CMP\Main\Source\Web\UI\JavaScript\knockoutExtensions.js"
+ 
---
+++ Paranoias/Paranoia Maps Tests/CopyToPath_Files.bat
@@ -0,0 +1,2 @@
+xcopy /Y  "C:\Projects\My_Webpack_Base_1\Paranoias\Paranoia Maps Tests\MapDrawing.js" "C:\Workspace\CMP\Main\Source\Web\UI\Assets\Javascript\Maps\MapDrawing.js"
+xcopy /Y  "C:\Projects\My_Webpack_Base_1\Paranoias\Paranoia Maps Tests\knockoutExtensions.js" "C:\Workspace\CMP\Main\Source\Web\UI\JavaScript\knockoutExtensions.js"
