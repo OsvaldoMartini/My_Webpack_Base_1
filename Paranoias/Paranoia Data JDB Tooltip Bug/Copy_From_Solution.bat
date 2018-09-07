8561ed3b738c06171aa45e164bb3b0a6e547d35c
Osvaldo Martini <OsvaldoMartini@users.noreply.github.com>
2018-09-07 16:00:24 +0100

    Paranoia tooltip

---
+++ Paranoias/Paranoia Data JDB Tooltip Bug/Copy_From_Solution.bat
@@ -0,0 +1,3 @@
+xcopy /Y "C:\Workspace\CMP\Main-04.07\Source\Web\UI\Views\AandD\JDB\DefenceBudgets.js"
+xcopy /Y "C:\Workspace\CMP\Main-04.07\Source\Web\UI\Views\AandD\JDB\Lens.cshtml"
+xcopy /Y "C:\Workspace\CMP\Main-04.07\Source\Config\JANESControlConfig.xml" 
