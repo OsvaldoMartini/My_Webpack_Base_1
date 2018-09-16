4306fab8008c12410cd1862e6862070f18278801
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-09-16 18:08:40 +0100

    Kncokout updates

--- AspAndWebpack/Vortex_3D/main.js
+++ AspAndWebpack/Vortex_3D/main.js
@@ -1,19 +1,25 @@
-
 console.log('main');
-//import './content/lib/jquery';
-//import ko from './content/lib/knockout';
+import $ from './content/lib/jquery.js';
+import * as jQuery from './content/lib/jquery.js';
+import ko from './content/lib/knockout.js';
+
+//import * as knockout from './content/lib/knockout.js';
+
+//import jQuery from "./content/lib/jquery.js";
+//window.$ = window.jQuery = jQuery;
 
 class AppViewModel {
     constructor() {
         this.chosenSector = ko.observable('all');
         this.monthsData = ko.observable();
+        console.log('refreshData');
 
         ko.computed(() => this.refreshData());
     }
 
     refreshData() {
         var sector = this.chosenSector();
-        var url = 'http://www.wservices.co.uk/services/GeoService.svc/Backend/${sector}.json';
+        var url = 'http://www.wservices.co.uk/services/GeoService.svc/Backend/${sector}';
         
         console.log('refreshData');
 
