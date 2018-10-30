9eb59d16db2a087b8a7802722026f192238f1f78
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-10-30 21:19:10 +0000

    More Teste with Toggle

--- AspAndWebpack/JavaScript/Add-Element-Click.js
+++ AspAndWebpack/JavaScript/Add-Element-Click.js
@@ -6,5 +6,25 @@ base.addEventListener('click', function(event) {
   let closest = event.target.closest(selector);
 	if (closest && base.contains(closest)) {
   	closest.classList.add('blue');
-  }
+   }
+
 });
+
+
+function toggle_Class() {
+  var element = document.getElementById("middle-panel");
+  //var element = document.getElementsByClassName("middle-panel");
+  
+  if (element.classList) { 
+      element.classList.toggle("resize");
+  } else {
+      // var classes = element.className.split(" ");
+      // var i = classes.indexOf("resize");
+
+      // if (i >= 0) 
+      //     classes.splice(i, 1);
+      // else 
+      //     classes.push("resize");
+      //     element.className = classes.join(" "); 
+  }
+}
