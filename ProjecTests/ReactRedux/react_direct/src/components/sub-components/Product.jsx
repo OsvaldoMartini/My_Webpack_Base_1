e4fe4e2fd9e48bf0f2d50f3c605510f90fb844a0
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-03-26 22:44:15 +0000

    Planet Counter

--- ProjecTests/ReactRedux/react_direct/src/components/sub-components/Product.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/sub-components/Product.jsx
@@ -13,18 +13,25 @@ export default class Product  extends React.Component {
 
      //TODO-MARTINI Borer for the Each Product class='w3-container w3-border w3-round-xlarge'
   
-     ShowProductDetails(){
-        //alert('Need to be fixed');
-        this.props.handleShowProductDetails(this.props.name);
-     }
+    //  ShowProductDetails(){
+    //     //alert('Need to be fixed');
+    //     this.props.handleShowProductDetails(this.props.name);
+    //  }
+
+
+     ShowProductDetails = () => {
+        //var lang = this.dropdown.value;
+        this.props.handleShowProductDetails(this.props.name);            
+    }
     render(){
         return (
             <div>
             <p>{this.props.name} - ${this.props.price}</p>
             <button onClick={() => this.handleBuy()}>Buy</button>
             <button onClick={() => this.ShowProductDetails}>Show Me</button>
+            {/* onClick={this.ShowProductDetails} */}
             <h3>Qty: {this.state.qty} item(s)</h3>
             </div>
          );
     }
-}
+}   
