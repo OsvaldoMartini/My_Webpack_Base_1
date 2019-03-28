df349766a2243bf903980a9830017979bfd111af
Osvaldo Martini <Osvaldo.Martini@gmail.com>
2019-03-28 09:05:16 +0000

    Starting SearchBar components

---
+++ ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar.jsx';
+
+const AppSearch = () => {
+
+    return (
+        <div className="AppSearch">
+            <SearchBar />
+        </div>
+    );
+}
+
+export default AppSearch;
