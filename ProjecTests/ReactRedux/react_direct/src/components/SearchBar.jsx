9b1b8b689754390269711237872b59647c6be711
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-03-29 11:13:32 +0000

    1) BIDING the context, for solving "CONTEXT" problem when whe get "this === undefined"

--- ProjecTests/ReactRedux/react_direct/src/components/SearchBar.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/SearchBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: 'Hi there!' };
+ // 1) BIDING the context, for solving "CONTEXT" problem when whe get "this === undefined"
+ constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+}
+
+    state = { term: 'Hi there!' };
 
 onInputChange(event){
     console.log(event.target.value);
