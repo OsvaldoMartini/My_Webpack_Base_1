5e7f9f68c97c3f1e4f7dd8f2d331d0f9ca62d830
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-03-29 11:08:56 +0000

    // 1) BIDING the Solving CONTEXT PROBLE when "this === undegined"

--- ProjecTests/ReactRedux/react_direct/src/components/SearchBar.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/SearchBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: 'Hi there!' };
+ // 1) BIDING the Solving CONTEXT PROBLE when "this === undegined"
+ constructor(props) {
+     super(props);
+     this.onFormSubmit = this.onFormSubmit.bind(this);
+ }
+ 
+ state = { term: 'Hi there!' };
 
 onInputChange(event){
     console.log(event.target.value);
