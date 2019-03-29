ed890e22586f86552746489c157ccc1e951b07d0
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-03-29 17:52:43 +0000

    axios callback

--- ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
+++ ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
@@ -6,12 +6,14 @@ class AppSearch extends React.Component {
   onSearchSubmit(term) {
 
     console.log(term);
-    
+
     axios.get('https://api.unsplash.com/search/photos', {
         params: { query: term },
         headers: {
             Authorization: 'Client-ID 43d68399a648aab9b8b72502c3feddddabbc9399d2d5f0cdd2df7a284d27c64f'
         }
+    }).then(response => {
+        console.log(response.data.results);
     });
   }
 
