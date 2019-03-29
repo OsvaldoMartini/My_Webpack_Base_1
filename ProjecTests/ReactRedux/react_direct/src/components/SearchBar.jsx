b21ce92654f1c55b97e4a0d7c497bd0317c27c89
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-03-29 11:37:36 +0000

    BIDING the context, for solving "CONTEXT"  as the "Chosen One"

--- ProjecTests/ReactRedux/react_direct/src/components/SearchBar.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/SearchBar.jsx
@@ -1,43 +1,48 @@
 import React from "react";
+//1) Tag: Binding_Context : BIDING the context, for solving "CONTEXT" problem when whe get "this === undefined"
+//2) Tag: Arrowfunction_OnFormSubmit : ARROW FUNCTION the context, for solving "CONTEXT" problem when whe get "this === undefined"
+//3) Tag: Arrowfunction_Render_1 : ARROW FUNCTION the context, for solving "CONTEXT" problem when whe get "this === undefined"
+//4) Tag: Arrowfunction_Render_1 : ARROW FUNCTION the context, for solving "CONTEXT" problem when whe get "this === undefined"
 
 class SearchBar extends React.Component {
- state = { term: 'Hi there!' };
+  constructor(props) {
+      super(props);
+      this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
 
-onInputChange(event){
+  state = { term: "Hi there!" };
+
+  onInputChange(event) {
     console.log(event.target.value);
-}
+  }
 
-onInputClick(){
-    console.log('Input was Clicked');
-}
+  onInputClick() {
+    console.log("Input was Clicked");
+  }
 
-//Here Have a Coxtext Problem (this === UNDEFINED)
-onFormSubmit() {
+  //Here Have a Coxtext Problem (this === UNDEFINED)
+  onFormSubmit(event) {
     event.preventDefault();
 
     console.log(this.state.term);
-}
+  }
 
-//4) ARROW FUNCTION the context, for solving "CONTEXT" problem when whe get "this === undefined"
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={() => this.onFormSubmit()} className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>ImageSearch</label>
             <input
               type="text"
-              
               //Uncontrolled Element
               onClick={this.onInputClick}
-              
               //onChange={this.onInputChange}     //When we whant to send the evet foward some function
               //onChange={(event) => console.log(event.target.value)}   //When we Have only one line of Logic
 
-              
               //Controlled Element
-               value={this.state.term.toUpperCase()}
-               onChange={e => this.setState({ term: e.target.value })}
+              value={this.state.term.toUpperCase()}
+              onChange={e => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
