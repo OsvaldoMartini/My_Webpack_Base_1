7b387a6dfc04396f55a9d0a825795be55c15bf9d
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-03-29 17:22:42 +0000

    Axios Request

--- ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
+++ ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import SearchBar from '../components/SearchBar.jsx';
+import React from "react";
+import axios from "../plugins/axios.js";
+import SearchBar from "../components/SearchBar.jsx";
 
-const AppSearch = () => {
+class AppSearch extends React.Component {
+  onSearchSubmit(term) {
 
+    console.log(term);
+    
+    axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+            Authorization: 'Client-ID 43d68399a648aab9b8b72502c3feddddabbc9399d2d5f0cdd2df7a284d27c64f'
+        }
+    });
+  }
+
+  render() {
     return (
-        <div className="AppSearch" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
+      <div className="AppSearch" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
     );
+  }
 }
 
 export default AppSearch;
