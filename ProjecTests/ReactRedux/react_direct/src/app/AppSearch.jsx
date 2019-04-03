f1862e0a0fc60d5fc527a06fea51b743ca015d07
Osvaldo Martini <Osvaldo.Martini@gmail.com>
2019-04-03 15:46:00 +0100

    Turn the "async" into "Arrow function"

--- ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
+++ ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
@@ -5,7 +5,7 @@ import SearchBar from "../components/SearchBar.jsx";
 class AppSearch extends React.Component {
   state = {images: []}; // {images: []} Its is better because, If a have state.images.map this will work fine instead of {images: null} (that's cause error)
   
-    async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
   //console.log(term);
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         params: { query: term },
