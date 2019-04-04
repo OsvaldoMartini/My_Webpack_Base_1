4428b64644a18316d9690c4aeb0560901be2097a
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-04 21:20:03 +0100

    Creating Custom Clients -cleaning up the code Separating axios call

---
+++ ProjecTests/ReactRedux/react_direct/src/api/unsplash.jsx
@@ -0,0 +1,8 @@
+import axios from "../plugins/axios.js";
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID 43d68399a648aab9b8b72502c3feddddabbc9399d2d5f0cdd2df7a284d27c64f'
+    }
+});
--- ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
+++ ProjecTests/ReactRedux/react_direct/src/app/AppSearch.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import axios from "../plugins/axios.js";
+import unsplash from '../api/unsplash.jsx';
 import SearchBar from "../components/SearchBar.jsx";
 
 class AppSearch extends React.Component {
   state = {images: []}; // {images: []} Its is better because, If a have state.images.map this will work fine instead of {images: null} (that's cause error)
   
-  onSearchSubmit = async (term) => { /* this was converted to  arrow function to solve context problem */
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-            Authorization: 'Client-ID 43d68399a648aab9b8b72502c3feddddabbc9399d2d5f0cdd2df7a284d27c64f'
-        }
+  onSearchSubmit = async term => { /* this was converted to  arrow function to solve context problem */
+    const response = await unsplash.get('/search/photos', {
+        params: { query: term }
     });
 
     console.log(this);  {/* this bring us the actual this Object that is "onSubmit" */}
@@ -23,7 +20,7 @@ class AppSearch extends React.Component {
     return (
       <div className="AppSearch" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} guessWhoIam={this}/>
-        Found: {this.state.images.length}   {/* Uncaught (in promise) TypeError: this.setState is not a function */}
+        Found: {this.state.images.length}  images   {/* Uncaught (in promise) TypeError: this.setState is not a function */}
       </div>
     );
   }
