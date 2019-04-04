f38345353da3d888921f82587aa454fa393e60ef
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-04 21:43:26 +0100

    Second Way to Render -> Better to work with styles

--- ProjecTests/ReactRedux/react_direct/src/components/ImageList.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/ImageList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    return props.images.map((image) => {
+    const images = props.images.map((image) => {
         return <img src={image.urls.regular}/>
     });
 
+    return <div>{images}</div>;
 }
 
 export default ImageList;
