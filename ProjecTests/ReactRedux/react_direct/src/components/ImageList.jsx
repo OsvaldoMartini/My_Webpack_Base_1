c7ebf577f7aec2d85916e39433e05e776007ac31
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-04 21:40:42 +0100

    Returning list of Images First way to do it.

--- ProjecTests/ReactRedux/react_direct/src/components/ImageList.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/ImageList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);
+    return props.images.map((image) => {
+        return <img src={image.urls.regular}/>
+    });
 
-    return <div>ImageList</div>;
 }
 
 export default ImageList;
