70ef6861b006e0d48005ca5bfb3de6247e89d877
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-09 21:49:32 +0100

    Creating ImageCard Component to render each element individualy

--- ProjecTests/ReactRedux/react_direct/src/components/ImageList.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/ImageList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 //import style from '../css/ImageList.css';
+import ImageCard from './ImageCard.jsx';
 
 const ImageList = (props) => {
-    const images = props.images.map(({description, id, urls}) => {
-        return <img key={id} alt={description} src={urls.regular}/>
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image}/>
     });
 
     return <div className="image-list">{images}</div>;
