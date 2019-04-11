0f5839ef4782d07c718d63bd6cb5b2d7d9ae4383
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 18:11:40 +0100

    Small typo

--- ProjecTests/ReactRedux/react_direct/src/components/sub-components/VideoItem.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/sub-components/VideoItem.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const VideoItem = ({video, onVideoSelect}) => {
     //props.video  => destructure  => ({video})
     return (
-        // "onClick={onVideoSelect}"" will be call without the apropriate video item
+        // "onClick={onVideoSelect}" will be call without the apropriate video item
         // it needs o be arrow function to call the "onVideoSelect" function passing the apropriate video item as parameter
     <div onClick={() => onVideoSelect(video)} className="video-item item">
         <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
