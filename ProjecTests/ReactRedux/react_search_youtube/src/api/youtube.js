a8fb2d768e61114ea2c8a2012bc9b24feb211e1c
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 20:35:40 +0100

    Folders separation
    Separated per app

---
+++ ProjecTests/ReactRedux/react_search_youtube/src/api/youtube.js
@@ -0,0 +1,12 @@
+import axios from '../../plugins/axios.js';
+
+const KEY = 'AIzaSyCphzAQ2QLeu-mNSCYRiyydozqKlCXsEnw'; //martini.de.architect@gmail.com developers google api
+
+export default axios.create ({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
---
+++ ProjecTests/ReactRedux/react_tests/src/components/sub-components/ScrollBarDin.jsx
@@ -0,0 +1,80 @@
+import React, { Component }  from 'react';
+import {Scrollbar} from '../../../plugins/react_components/react-custom-scrollbars.js';
+
+// export default class ScrollBarDin extends React.Component
+// {
+//     render() {
+//         return (
+//                 <Scrollbar style={ {width: '100%', minHeight: 300} } >
+//                     <p>Hello world!</p>
+//                 </Scrollbar>
+//         );
+//     }
+// }
+
+
+// import $ from '../../plugins/jquery-v3-2-1/jquery.min.js';
+
+// export default class MyHomeComponent extends React.Component {
+//   componentDidMount() {
+//     const MCSB = require('malihu-custom-scrollbar-plugin');;
+//     let mcsb = new MCSB.default();
+
+//     $("#mypage").mCustomScrollbar({theme:'dark'});
+
+//   }
+//   render() {
+//     return (<div id="mypage">Enter a lot of content here..</div>);
+//   }
+
+// }
+
+export default class ScrollBarDin extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  
+    const array = [...Array(10000)].map((val, i) => `Item ${i}`);
+    console.log(array); // 1443535752
+
+    this.state = {
+      items: [
+        {
+          text: "id1",
+          id: 1
+        },
+        {
+          text: "id2",
+          id: 2
+        }
+      ]
+    };
+
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <div className="left-col">Left col</div>
+
+        <div className="center-col">
+          <span>List</span>
+          <ul>
+          {/* {this.state.items.map((item, index) => (
+            <li
+              {...item}
+              key={index}
+            />
+          )
+          )} */}
+
+            {this.state.items.map((item, index) => (
+              <li key={`item_${index}`}>{item.text}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="right-col">Right col</div>
+      </div>
+    );
+  }
+}
