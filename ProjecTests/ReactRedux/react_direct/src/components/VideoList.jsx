6b0d3114ace8e1cd3c8ac0426f49e09a3aabffca
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 17:56:37 +0100

    Do Structure Out and passing down between VideoList and VideoItem. More One Level down.

--- ProjecTests/ReactRedux/react_direct/src/components/VideoList.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/VideoList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from '../components/sub-components/VideoItem.jsx';
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
     //props.videos
 
     const renderedList = videos.map(video => {
-        return <VideoItem video={video}/>
+        return <VideoItem onVideoSelect={this.onVideoSelect} video={video}/>
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div>;
