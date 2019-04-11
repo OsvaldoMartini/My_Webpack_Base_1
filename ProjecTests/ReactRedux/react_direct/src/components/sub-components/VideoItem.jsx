9f87091f9f1777eedb84b2be07b94445c05b93e9
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 18:08:41 +0100

    Last Level of the Deeply Nested Callback. Do structure Out and onClick event to return the video object. Important: This way will be call without the apropriate video item -> onClick={onVideoSelect}      It needs o be arrow function to call the "onVideoSelect" function passing the apropriate video item as parameter: -> onClick={() => onVideoSelect(video)}

--- ProjecTests/ReactRedux/react_direct/src/components/sub-components/VideoItem.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/sub-components/VideoItem.jsx
@@ -1,10 +1,12 @@
 //import '../../css/VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
     //props.video  => destructure  => ({video})
     return (
-    <div className="video-item item">
+        // "onClick={onVideoSelect}"" will be call without the apropriate video item
+        // it needs o be arrow function to call the "onVideoSelect" function passing the apropriate video item as parameter
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
         <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
         <div className="content"> 
            <div className="header">
