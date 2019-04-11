494751d7a3c002cd4ab275f598f23910dba97c0f
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 19:30:22 +0100

    Avoind Warnings and Erros Messages: Including "key" on VideoItem => VideoList.jsx component Including "title" on IFrame => VideoDetail.jsx component Including "alt" on image (img) => VideoItem.jsx component

--- ProjecTests/ReactRedux/react_direct/src/components/VideoDetail.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/VideoDetail.jsx
@@ -12,7 +12,7 @@ const VideoDetail = ({ video }) => {
         {/* I-Frame Tag is going to attempt to make a request to some outside Web site besides the one that the user is currently visiting on the screen.
         This I-Frame element it will make a request on its own without any Ajax stuff or anything like that. */}
         <div className="ui embed">
-        <iframe src={videoSrc} />
+        <iframe title="video player" src={videoSrc} />
         </div>
         <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
