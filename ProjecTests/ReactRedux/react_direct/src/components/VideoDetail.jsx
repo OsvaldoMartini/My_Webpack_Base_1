23ae6218f3aa954e9f0f702143e6db19e3154a29
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 19:17:42 +0100

    I-Frame Tag is going to attempt to make a request to some outside Web site besides the one that the user is currently visiting on the screen. This I-Frame element it will make a request on its own without any Ajax stuff or anything like that.

--- ProjecTests/ReactRedux/react_direct/src/components/VideoDetail.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/VideoDetail.jsx
@@ -5,8 +5,15 @@ const VideoDetail = ({ video }) => {
         return <div>Loading...</div>
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
     <div>
+        {/* I-Frame Tag is going to attempt to make a request to some outside Web site besides the one that the user is currently visiting on the screen.
+        This I-Frame element it will make a request on its own without any Ajax stuff or anything like that. */}
+        <div className="ui embed">
+        <iframe src={videoSrc} />
+        </div>
         <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
