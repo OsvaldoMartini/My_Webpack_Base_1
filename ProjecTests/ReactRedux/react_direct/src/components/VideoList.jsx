f152e8605ab3897e3d07a830246bddc8756119f0
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 17:57:50 +0100

    No Need the "this" statment in this case.

--- ProjecTests/ReactRedux/react_direct/src/components/VideoList.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/VideoList.jsx
@@ -5,7 +5,7 @@ const VideoList = ({videos, onVideoSelect}) => {
     //props.videos
 
     const renderedList = videos.map(video => {
-        return <VideoItem onVideoSelect={this.onVideoSelect} video={video}/>
+        return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div>;
