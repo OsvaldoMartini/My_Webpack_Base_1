246a1f43f711f81bc1bfb419eb050853bbbc27c2
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-11 13:50:05 +0100

    Mapping each VideoItem and calling the VideoItem render() => renderedList

--- ProjecTests/ReactRedux/react_direct/src/components/VideoList.jsx
+++ ProjecTests/ReactRedux/react_direct/src/components/VideoList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoItem from '../components/sub-components/VideoItem.jsx';
 
 const VideoList = ({videos}) => {
     //props.videos
 
-    return <div>{videos.length}</div>;
+    const renderedList = videos.map((videos) => {
+        return <VideoItem />
+    });
+
+    return <div>{renderedList}</div>;
 }
 
 export default VideoList;
