d18abceba98fa0911cd673335936ba9d3698d8ab
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-14 15:48:43 +0100

    REDUCERS

--- ProjecTests/ReactRedux/react_redux/src/api/reduxApi.jsx
+++ ProjecTests/ReactRedux/react_redux/src/api/reduxApi.jsx
@@ -33,3 +33,22 @@ const createClaim = (name, amountOfMoneyToCollect) => {
 
 //  Every single "Creator" returns a plain javascript object which refer to as an action 
 //  And an action has a "type" and "payload"
+
+/** ####  REDUCERS  ###    */
+
+//  Reducers (Departments!) 
+
+const claimsHistory = (oldListOfClaims, action) => {
+    if (action.type === 'CREATE_CLAIM'){
+      //WE CARE ABOUT THIS ACTION (fomr!)
+      //[... get allthe records inside there (ES2015)
+      //And Add to a New Brand Array  !! Verry Important
+      //REDUX Is all writTe Correctly BY ALWAYS CREATING NEW ARRAYS
+      // oldListOfClaims.push(action.payload); //It Modifies the current array
+      
+      return [...oldListOfClaims, action.payload]
+    }
+    
+    //we don't care the acrion (form!)
+    return oldListOfClaims
+  };
