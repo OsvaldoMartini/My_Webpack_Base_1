7e426224cc9e8858596b9f67c2e9dcd6a724aea1
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-15 21:54:24 +0100

    It means "imported the Named export File" selectSong

--- ProjecTests/ReactRedux/react_redux_songs/src/components/AppSongs.js
+++ ProjecTests/ReactRedux/react_redux_songs/src/components/AppSongs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //Just Sample to Import Arrow Function "export" withut the keyworld "default"
-import {selectSong} from '../actions/index.js';
+import {selectSong} from '../actions/index.js'; //It means "imported the Named export File" selectSong
 
 const App = () => {
     return <div>App Songs</div>
