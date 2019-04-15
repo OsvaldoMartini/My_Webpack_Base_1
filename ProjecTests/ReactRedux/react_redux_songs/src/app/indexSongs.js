dbf554672588df7fb0d9e619851e78cc4277618e
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-15 22:45:01 +0100

    Now the last thing I'd (I would)  do is create an instance of the provider and wrap it. Our app component with it. Wrapping the Provider with our app and passing a single "prop" called store The Store is going to be the result of calling create store and passing in our reducers

--- ProjecTests/ReactRedux/react_redux_songs/src/app/indexSongs.js
+++ ProjecTests/ReactRedux/react_redux_songs/src/app/indexSongs.js
@@ -9,7 +9,17 @@ import AppSongs from '../components/AppSongs.js';
 //Underneath my App a Import the Reducers
 import reducers from '../reducers/index.js';  //In case the Use of Web Pask can be write only as '../reducers'
 
-ReactDOM.render(<AppSongs/>, document.querySelector('#root'));
+//Wrapping the Provider with our app and passing a single "prop" called store
+// The Store is going to be the result of calling create store and passing in our reducers
+// We don't usually mess around with the store directly
+//Instead we pass it off to this provider and the provider essentially takes care of everything from there
+// Typical Redux application you're actually very rarely going to interact directly with the store
+// Outside of some more advanced cases we do it.
+ReactDOM.render(
+<Provider store={createStore(reducers)}>
+    <AppSongs/>
+</Provider>, 
+document.querySelector('#root'));
 
 // Now Our Goal is to make sure that we get that provider tag at the very top
 // of our component hierarchy and
