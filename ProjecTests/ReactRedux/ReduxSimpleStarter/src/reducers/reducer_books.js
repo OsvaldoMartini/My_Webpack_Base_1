91dbc8b9066f2810ec38caa20b7f10cce71efe88
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-19 11:49:49 +0100

    Reducers is just a function that returns a pieces of the application states Combining Reducer into  React App

---
+++ ProjecTests/ReactRedux/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title:'Turotial Java'},
+        {title:'Harry Potter'},
+        {title:'The Dark Tower'},
+        {title:'The Dart Vayder'},
+        {title:'eloquent Ruby'}
+    ]
+}
