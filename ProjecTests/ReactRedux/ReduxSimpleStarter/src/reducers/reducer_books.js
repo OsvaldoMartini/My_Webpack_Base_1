ddf6a280a59260bb57badfd02ab34c6301092a5a
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-19 20:51:36 +0100

    More Properties

--- ProjecTests/ReactRedux/ReduxSimpleStarter/src/reducers/reducer_books.js
+++ ProjecTests/ReactRedux/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        {title:'Turotial Java'},
-        {title:'Harry Potter'},
-        {title:'The Dark Tower'},
-        {title:'The Dart Vayder'},
-        {title:'eloquent Ruby'}
+        {title:'Turotial Java', pages: 101},
+        {title:'Harry Potter', pages: 39},
+        {title:'The Dark Tower', pages: 85},
+        {title:'The Dart Vayder', pages: 345},
+        {title:'eloquent Ruby', pages: 1}
     ]
 }
