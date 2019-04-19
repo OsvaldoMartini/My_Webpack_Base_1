a02b9c716ee5c641fe354bf81a3b44597b920636
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-19 13:08:20 +0100

    We use a separated library called React-Redux to forge a connection there. Whenever we forge a connection between a component and redux it turns that component into a "container" or "smart component" depending on what terminology you want to use.

--- ProjecTests/ReactRedux/ReduxSimpleStarter/src/containers/book-list.js
+++ ProjecTests/ReactRedux/ReduxSimpleStarter/src/containers/book-list.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 
+//TO FORGE the coneection between the React and Reducers
+// TEH GLUE BETWEEN REACT AND REDUX
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+//Not Export "export default" We just need the Classe Base Component
+ class BookList extends Component {
     //We are going to Pretend
     //We're going to plan that we're going to wire up the list of books to be
     //on this dot props for the booklist and we'll map over that array
     //and for each element int the array will return an ("ally"/"l ie") "<li>"
     //Don't forget the "key" because is a list use any unique value
     renderList() {
-        return this.props.book.map((book) => {
+        return this.props.books.map((book) => {
             return <li key={book.title} className="list-group-item">{book.title}</li>
         });
     }
     
     render () {
+        console.log(this.props.asdf);
         return (
             <ul className="list-group col-sm-4">
             {this.renderList()}
@@ -21,3 +26,26 @@ export default class BookList extends Component {
         )
     }
 }
+
+//the Purpose of this is to take our application state as an argument
+function mapStateToProps(state) {
+    //Whatever is returned will show up as props 
+    //insideof BookList
+    return {
+        asdf: '123',
+        books: state.books
+    }
+}
+
+
+//React-Redux is the Glue between both
+
+//At The Very bottom We need to actually make use of that connect function
+// that we imported up to the connect and Return a Container
+// We don't want to export the BookList plane component that nobody cares about
+
+// Connect: Takes a function and component and produces a container (Smart component)
+// ####  WE WANT TO EXPORT THE CONTAINER  ### //
+export default connect(mapStateToProps)(BookList);
+
+
