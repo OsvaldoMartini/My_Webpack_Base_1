2c1e7064e7e1245033af13b1fb6c9e3aac124050
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 11:15:59 +0100

    Defining the ROOT_URL and testing the forecast API_KEY

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
@@ -1,10 +1,14 @@
 const API_KEY = 'e6b5ae2998d50eb8dc414bef80eef5bf';
+//Using Back Ticks  ES6 Syntax
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`;
 
 // Keeping our ACTION TYPES consistent between our ACTION CREATORS and REDUCERS
 // We can just jave single canonica source of action type right here
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather() {
+    const url = `${ROOT_URL}&q=${city},us`;
+
     return {
         type: FETCH_WEATHER
     }
