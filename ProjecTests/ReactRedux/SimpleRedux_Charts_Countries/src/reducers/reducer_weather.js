a71afdf233ade9d43c6124e7c3b84a3aa0a40adb
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 18:01:01 +0100

    Usage Of: THIS USAGE IS CORRECT: return [action.payload.data]; THIS USAGE IS CORRECT: return [action.payload.data, ...state]; THIS USAGE IS CORRECT: return state.concat([action.payload.data]); Incorrect Usage of: THIS USAGE IS NOT CORRECT: return state.push(action.payload.data); (SUBLE TRAP)

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/reducers/reducer_weather.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/reducers/reducer_weather.js
@@ -1,5 +1,32 @@
-export default function (state = null, action) {
+import { FETCH_WEATER } from '../actions/index';
+//Expecting Array state = [] Initialization
+export default function (state = [], action) {
     console.log('Action Received', action);
+    switch (action.type) {
+        case (FETCH_WEATER):
+            //We only have the concept of one searchable city one active city at a time
+            //TODO: THIS DO NOT COLLECT CITIES OVER TIME 
+            //return [action.payload.data];  //Test only not create a list or searcheable, just one 
+
+            // ### SUBLE TRAPS DON'T DO IT !!!!!
+            // ### THIS LINE OF CODE IS OPENING UP A HUGE CAN OF WORMS  => "... state.push(action ...."
+            // THIS IS MUTATING THE ARRAY NEVER DO IT IT INSIDE OF REDUX USE "...CONCAT..."
+            //return state.push(action.payload.data); (SUBLE TRAP)
+
+
+
+            // THIS IS TOPIC OF STATE AND ROCK COMPONENT
+            // => THE TWO KNOW WAYS TO DO IT IS UNDERNIF
+            // => this return new version of the OLD  STATE array
+            //TODO: THIS COLLECT CITIES OVER TIME 
+            //return state.concat([action.payload.data]);  //THIS IS CORRECT
+
+            //  OR 
+            // => other approach this end Up with Kind of array [city,city,city,.....] and Not [city, [city, city]]
+            //TODO: THIS COLLECT CITIES OVER TIME 
+            return [action.payload.data, ...state];  //THIS IS CORRECT
+
+    }
     return state;
 }
 
