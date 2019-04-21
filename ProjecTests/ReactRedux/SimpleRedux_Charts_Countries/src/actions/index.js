1bdcc039f4c95c706366bc00f76cbe61e756e8a4
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 08:43:11 +0100

    Keeping our ACTION TYPES consistent between our ACTION CREATORS and REDUCERS We can just jave single canonica source of action type right here

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
@@ -1,1 +1,11 @@
 const API_KEY = 'e6b5ae2998d50eb8dc414bef80eef5bf';
+
+// Keeping our ACTION TYPES consistent between our ACTION CREATORS and REDUCERS
+// We can just jave single canonica source of action type right here
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    }
+}
