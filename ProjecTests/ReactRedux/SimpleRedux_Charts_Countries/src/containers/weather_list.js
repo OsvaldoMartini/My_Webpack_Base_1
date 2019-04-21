c7e949835b10da3c2d6c9e33c2a98a567e90007d
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 19:44:24 +0100

    A Little Clean Up after ES6 Syntax

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/containers/weather_list.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/containers/weather_list.js
@@ -21,9 +21,5 @@ export default class WeatherList extends Component {
 
 // This it will get the data weather from the "combineReducers" and define insinde our WeatherList Container component
 function mapStateToProps({ weather }) {  // => We could use just "weather" Instead state.weather (ES6 Syntax)
-    //Same thing to do: 
-    //const weather = state.weather;"
-    //And We Could condense more like:
-    //return { weather: weather }; //We could shrink it to just curly braces weather (because the both are identical) 
     return { weather }; // { weather } === { weather: weather };
 }
