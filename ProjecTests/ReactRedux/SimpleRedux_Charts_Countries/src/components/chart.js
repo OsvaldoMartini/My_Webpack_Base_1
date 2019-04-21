6feecac039792995270e596858bc1df2b0f1d7a2
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 22:23:25 +0100

    Create Total Avarage Using "LODASH" already included. Utility library to calculate average (UNDERSCORE) to represent lodash already inlcuded in the Initial package Or Install direct from npm terminal: npm install --save lodash

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/chart.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/chart.js
@@ -1,11 +1,19 @@
+import _ from 'lodash';  //=> Utility library to calculate average (UNDERSCORE) to represent lodash already inlcuded in the Initial package
 import React from 'react';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+function average(data) {
+    return _.round(_.sum(data) / data.length);
+}
+
 export default (props) => {
     return (
-        <Sparklines heigth={120} width={180} data={props.data}>
-            <SparklinesLine color={props.color} />
-            <SparklinesReferenceLine type="avg" />
-        </Sparklines>
+        <div>
+            <Sparklines heigth={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)}</div>
+        </div>
     );
 }
