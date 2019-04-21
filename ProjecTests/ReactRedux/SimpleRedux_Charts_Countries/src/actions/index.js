64702e94dfd93486e43fb163d024ac59479845f3
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 13:58:46 +0100

    Correcting the BaseURL

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'e6b5ae2998d50eb8dc414bef80eef5bf';
 //Using Back Ticks  ES6 Syntax
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // Keeping our ACTION TYPES consistent between our ACTION CREATORS and REDUCERS
 // We can just jave single canonica source of action type right here
