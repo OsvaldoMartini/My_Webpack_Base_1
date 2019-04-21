4d1f924b1faf1edb85353dcb457d6146ef174dc6
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 21:27:17 +0100

    Clean Up

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/reducers/reducer_weather.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/reducers/reducer_weather.js
@@ -1,7 +1,7 @@
 import { FETCH_WEATHER } from '../actions/index';
 //Expecting Array state = [] Initialization
 export default function (state = [], action) {
-    console.log('Action Received', action);
+    //console.log('Action Received', action);
     switch (action.type) {
         case (FETCH_WEATHER):
             //We only have the concept of one searchable city one active city at a time
