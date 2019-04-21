9a7f2d9058deb6df26dc87b9c61cbf00121638ad
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 11:32:44 +0100

    Making Request with AXIOS npm install --save axios

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = 'e6b5ae2998d50eb8dc414bef80eef5bf';
 //Using Back Ticks  ES6 Syntax
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`;
@@ -8,8 +10,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather() {
     const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
 
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     }
 }
