3a472d73f6c7913a0d1d109f15388611da7d3547
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-21 18:26:51 +0100

    WeaterList as a Container because it will make use of Redux State Data

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/app.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Component } from 'react';
 
 import SeachBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SeachBar />
+        <WeatherList />
       </div>
     );
   }
