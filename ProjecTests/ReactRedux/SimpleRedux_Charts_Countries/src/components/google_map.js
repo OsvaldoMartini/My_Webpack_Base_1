f7f637599cee9decd868151ba56f1ab712424181
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-22 08:29:46 +0100

    "Ref" tag giv me a direct reference to any conponent on Screen

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/google_map.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/google_map.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
-
+    render() {
+        //  this.refs.map It Gives Directy reference in Html content o the page
+        return <div ref="map" />;
+    }
 }
+
 
 export default GoogleMap;
