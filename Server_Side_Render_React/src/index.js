c3287ecc4fb4d0c31cb72b9ce29d213cbd8fabd4
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-22 19:03:04 +0100

    Typo Correction

--- Server_Side_Render_React/src/index.js
+++ Server_Side_Render_React/src/index.js
@@ -1,7 +1,7 @@
 const express = requeire('express');
 const app = express();
 
-const React = require('reat');
+const React = require('react');
 const renderToString = require('react-dom/server').renderToString;
 const Home = require('./client/components/home').default;
 
