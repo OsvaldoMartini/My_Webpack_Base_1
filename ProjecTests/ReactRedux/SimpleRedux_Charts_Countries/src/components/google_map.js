02c67b72a33e51306a7b530991ab7bbc0bbf422b
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-22 11:19:25 +0100

    Work Around but still not working

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/google_map.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/google_map.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 class GoogleMap extends Component {
 
     componentDidMount() {
+        //Work Around to show the Google Map
+        const google = window.google;
+
         // this Came from index.html => "...<script src="https://maps.googleapis.com/maps/api/....."
         new google.maps.Map(this.refs.map, {
             zoom: 12,
