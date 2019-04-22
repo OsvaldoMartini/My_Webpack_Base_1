9987cba052ae143087520fbc2f449b4baeffcb77
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-22 10:54:17 +0100

    Google Map as Component Thirdh Part Library

--- ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/google_map.js
+++ ProjecTests/ReactRedux/SimpleRedux_Charts_Countries/src/components/google_map.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+
+    componentDidMount() {
+        // this Came from index.html => "...<script src="https://maps.googleapis.com/maps/api/....."
+        new google.maps.Map(this.refs.map, {
+            zoom: 12,
+            center: {
+                lat: this.props.lat,
+                lng: this.props.lon
+            }
+        });
+    }
+
+
     render() {
         //  this.refs.map It Gives Directy reference in Html content o the page
         return <div ref="map" />;
