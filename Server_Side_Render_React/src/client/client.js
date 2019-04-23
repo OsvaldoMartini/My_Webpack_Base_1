89527185ea73b32bd81cfe2a26642cb33924e3f6
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-23 08:04:52 +0100

    Client Bootup

--- Server_Side_Render_React/src/client/client.js
+++ Server_Side_Render_React/src/client/client.js
@@ -1,3 +1,8 @@
 // Startup point for the client side application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home';
 
 console.log('Hi there!');
+
+ReactDOM.render(<Home />, document.querySelector('root'));
