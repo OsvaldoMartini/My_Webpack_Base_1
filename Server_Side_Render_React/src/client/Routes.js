b7fdcc931f3928ee45ccbc9517ff2f285e41690f
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-27 14:22:17 +0100

    Adding the UsersList Component into the Routes

--- Server_Side_Render_React/src/client/Routes.js
+++ Server_Side_Render_React/src/client/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import UsersList from './components/UsersList';
 
 // Exact Prop I want to show this route if the URL is exactly the path "Slash"
 export default () => {
@@ -8,6 +9,7 @@ export default () => {
         <div>
             <Route exact path="/" component={Home} />
             <Route path="/hi" component={() => 'Hi'} />
+            <Route path="/users" component={UsersList} />
         </div>
     );
 }
