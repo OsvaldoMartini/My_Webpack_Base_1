ce78656d37795cbfc9a950d7be00afc177b256b0
Osvaldo.Martini <osvaldo.martini@gmail.com>
2019-04-27 06:43:15 +0100

    Passing Req into StaticRouter

--- Server_Side_Render_React/build/bundle.js
+++ Server_Side_Render_React/build/bundle.js
@@ -94,12 +94,20 @@ var app = (0, _express2.default)();
 //const renderToString = require('react-dom/server').renderToString;
 //const Home = require('./client/components/home').default;
 
-// ## This tells express that it needs to treat that public directory as a static or public directory that is
-// ## available o the outside world
 app.use(_express2.default.static('public'));
 
+// ## This tells express that it needs to treat that public directory as a static or public directory that is
+// ## available o the outside world
+// The BrowserRouter (Not The StaticRouter) BrowserRouter Has the ability to look directly at our browser's address bar to figure out what the current path is, and what set
+// of components it needs to show on the screen.
+// The StaticRouter however, needs to be told exactly what the current path is that it needs to consider.
+// So for us, we need to somehow communicate the current path that the user is trying to access to the StaticRouter, So that StaticRouter knows what set of components it should show on the screen.
+// The current path that it need to consider is contained in the original request object that express passed out to our Router Handler inside the JSX File.
+// The "(req)" inside of "...app.get('/', (req, res) ..." This request ("..req..") contains the URL that the user is trying to access.
+
+//Passing the " req " inside of the render as argument
 app.get('/', function (req, res) {
-    res.send((0, _renderer2.default)());
+    res.send((0, _renderer2.default)(req));
 });
 
 app.listen(3000, function () {
@@ -148,7 +156,7 @@ exports.default = function (req) {
     var content = (0, _server.renderToString)(_react2.default.createElement(
         _reactRouterDom.StaticRouter,
         { location: req.path, context: {} },
-        _react2.default.createElement(Router, null)
+        _react2.default.createElement(_Routes2.default, null)
     ));
 
     // ## Underneath a tine little HTML. I'll snifft it (farejar)
--- Server_Side_Render_React/src/index.js
+++ Server_Side_Render_React/src/index.js
@@ -9,18 +9,25 @@ const app = express();
 //const renderToString = require('react-dom/server').renderToString;
 //const Home = require('./client/components/home').default;
 
-// ## This tells express that it needs to treat that public directory as a static or public directory that is
-// ## available o the outside world
 app.use(express.static('public'));
 
+// ## This tells express that it needs to treat that public directory as a static or public directory that is
+// ## available o the outside world
+// The BrowserRouter (Not The StaticRouter) BrowserRouter Has the ability to look directly at our browser's address bar to figure out what the current path is, and what set
+// of components it needs to show on the screen.
+// The StaticRouter however, needs to be told exactly what the current path is that it needs to consider.
+// So for us, we need to somehow communicate the current path that the user is trying to access to the StaticRouter, So that StaticRouter knows what set of components it should show on the screen.
+// The current path that it need to consider is contained in the original request object that express passed out to our Router Handler inside the JSX File.
+// The "(req)" inside of "...app.get('/', (req, res) ..." This request ("..req..") contains the URL that the user is trying to access.
+
+//Passing the " req " inside of the render as argument
 app.get('/', (req, res) => {
-    res.send(renderer());
+    res.send(renderer(req));
 });
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
-
 
 //  Building:
 //  npm run dev:build:server
