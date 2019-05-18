082c4c62a52213f4466e21b0f4cce179cec67990
Osvaldo Martini <Osvaldo.Martini@gmail.com>
2019-05-18 14:59:03 +0100

    WebPack Code Spliting
    first Aproach for code spliting
    Our Code / Vendor Code
    webpack.optimize.CommonsChunkPlugin
    It Creates Separated Vendor.js file from Bundle.js

--- WebPack_React-Complete/webpack.config.js
+++ WebPack_React-Complete/webpack.config.js
@@ -1,10 +1,43 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'faker',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].js' //name key fro the entry section
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor' // Check the Dulplications modules and pull  in and out from bundle t reduce sizes
+    })
+  ]
 };
