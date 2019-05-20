2e5333cbe54d256b9e9456c847fd35c9a6aa8e59
Osvaldo Martini <Osvaldo.Martini@gmail.com>
2019-05-20 07:45:53 +0100

    React-Redux-j4 linebreak-style
    React-Redux-j4 linebreak-style

--- react-redux-js4/.eslintrc.js
+++ react-redux-js4/.eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-      "react/forbid-prop-types": 0,
-      "import/no-named-as-default": 0,
-      "no-param-reassign": ["error", { "props": false }],
-    },
-    "env": {
-      "browser": true,
-      "jest": true,
-    },
+  extends: 'airbnb',
+  rules: {
+    'react/forbid-prop-types': 0,
+    'import/no-named-as-default': 0,
+    'no-param-reassign': ['error', { props: false }],
+    'linebreak-style': ['error', 'windows']
+  },
+  env: {
+    browser: true,
+    jest: true
+  }
 };
