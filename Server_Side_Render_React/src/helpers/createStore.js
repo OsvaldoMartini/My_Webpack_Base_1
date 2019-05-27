dd47e73430536ecee487450cda3eebc2f249dc0a
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-05-27 17:18:12 +0100

    Deleting Folders

--- Server_Side_Render_React/src/helpers/createStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'; //Takecare about the Asynchronous call for the action creators
-import reducers from '../client/reducers'; //Getting all the Combined Reducers for the Creation of the Store
-
-export default () => {
-    const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-    return store;
-}
