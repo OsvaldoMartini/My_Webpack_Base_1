854171a08969a7aae53ec3dc9feb7253fc02827e
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-06-18 10:53:16 +0100

    Update run_knockout.js

--- Server_Side_Render_React/Comparator/app_knockout/run_knockout.js
+++ Server_Side_Render_React/Comparator/app_knockout/run_knockout.js
@@ -1,23 +1,46 @@
+var TestVM = function() {
+  var self = this;
+  self.selected = ko.observable();
+  self.data = ko.observableArray();
+};
+
+TestVM.prototype.select = function(item) {
+  this.selected(item.id);
+};
+
+TestVM.prototype.run = function() {
+  var data = _buildData(),
+    date = new Date();
+
+  this.selected(null);
+  this.data(data);
+  document.getElementById('run-knockout').innerHTML = new Date() - date + ' ms';
+};
+
 function _knockout() {
+  var testVM = new TestVM();
+
   ko.applyBindings(
-    {
-      selected: ko.observable(),
-      data: ko.observableArray(),
+    //   {
+    //     selected: ko.observable(),
+    //     data: ko.observableArray(),
 
-      select: function(item) {
-        this.selected(item.id);
-      },
+    //     select: function(item) {
+    //       this.selected(item.id);
+    //     },
 
-      run: function() {
-        var data = _buildData(),
-          date = new Date();
+    //     run: function() {
+    //       var data = _buildData(),
+    //         date = new Date();
 
-        this.selected(null);
-        this.data(data);
-        document.getElementById('run-knockout').innerHTML =
-          new Date() - date + ' ms';
-      }
-    },
+    //       this.selected(null);
+    //       this.data(data);
+    //       document.getElementById('run-knockout').innerHTML =
+    //         new Date() - date + ' ms';
+    //     }
+    //   },
+
+    testVM,
     document.getElementById('knockout')
   );
 }
