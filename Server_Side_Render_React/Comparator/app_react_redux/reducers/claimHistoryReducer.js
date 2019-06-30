e298e5a0b7418751ce425eb19815adfb64aa5416
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-06-30 12:12:56 +0100

    Clean Up

--- Server_Side_Render_React/Comparator/app_react_redux/reducers/claimHistoryReducer.js
+++
@@ -1,6 +0,0 @@
-const claimHistoryReducer = function(oldListOfClaims = [], action) {
-  if (action.type === 'CREATE_CLAIM') {
-    return [...oldListOfClaims, action.payload];
-  }
-  return oldListOfClaims;
-};
