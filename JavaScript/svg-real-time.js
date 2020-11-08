4056f447cef54a8f658a8ba21b7f16378413f0ce
Osvaldo Martini <OsvaldoMartini@users.noreply.github.com>
2020-11-08 17:58:38 +0100

    up

---
+++ JavaScript/svg-real-time.js
@@ -0,0 +1,18 @@
+var areaChartData = [
+	// The first layer
+	{
+		label: "Layer 1",
+		values: [
+			{ time: 1370044800, y: 100 },
+			{ time: 1370044801, y: 1000 }
+		]
+	},
+	// The second layer
+	{
+		label: "Layer 2",
+		values: [
+			{ time: 1370044800, y: 78 },
+			{ time: 1370044801, y: 98 }
+		]
+	}
+];
